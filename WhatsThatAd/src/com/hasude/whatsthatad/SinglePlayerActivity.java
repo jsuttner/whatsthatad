345715fec4f08eff3d27c633a2d1fa5799172e8b
Niklas Hatje <nhatje@gmx.de>
2014-11-09 00:57:44 -0800

    Twitter is das geilste Diggah!

--- WhatsThatAd/src/com/hasude/whatsthatad/SinglePlayerActivity.java
+++ WhatsThatAd/src/com/hasude/whatsthatad/SinglePlayerActivity.java
@@ -1,14 +1,32 @@
 package com.hasude.whatsthatad;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.graphics.Bitmap;
+import android.graphics.Bitmap.CompressFormat;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.TextView.OnEditorActionListener;
 import android.widget.Toast;
@@ -30,6 +48,11 @@ public class SinglePlayerActivity extends Activity{
 		EditText solutionEdit = (EditText) findViewById(R.id.SingleEditSolution);
 		TextView questionTV = (TextView) findViewById(R.id.SingleQuestionTextView);
 		questionImageView = (ImageView) findViewById(R.id.SingleQuestionImageView);
+		
+		//social media images
+		ImageView facebook = (ImageView) findViewById(R.id.SingleBtnFacebook);
+		ImageView google = (ImageView) findViewById(R.id.SingleBtnGoogle);
+		ImageView twitter = (ImageView) findViewById(R.id.SingleBtnTwitter);
 		
 		// load question and image
 		// TODO: for testing only
@@ -60,8 +83,8 @@ public class SinglePlayerActivity extends Activity{
 			        if (!event.isShiftPressed()) {
 			        	// the user is done typing. 
 			        	if(q.isAnswerCorrect(v.getText().toString())) {
-			        		questionImageView.setImageBitmap(q.getAdUncensored());
 			        		Toast.makeText(getApplicationContext(), "Yay, that was correct!", Toast.LENGTH_SHORT).show();
+			        		endTask();
 			        	} else {
 			        		Toast.makeText(getApplicationContext(), "Oh no, that was not correct. Try again!", Toast.LENGTH_SHORT).show();
 			        	}
@@ -72,7 +95,116 @@ public class SinglePlayerActivity extends Activity{
 			}
 		});
 		
+		// onClick listeners social media
+		facebook.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent shareIntent = new Intent(android.content.Intent.ACTION_SEND);
+				shareIntent.setType("text/plain");
+				shareIntent.putExtra(android.content.Intent.EXTRA_TEXT, "I need ya help biatch!");
+				shareIntent.putExtra(Intent.EXTRA_STREAM, storeImage(q.getAdCensored()));
+
+				PackageManager pm = v.getContext().getPackageManager();
+				List<ResolveInfo> activityList = pm.queryIntentActivities(shareIntent, 0);
+			    for (final ResolveInfo app : activityList){
+			         if ((app.activityInfo.name).contains("facebook")){
+			        	 final ActivityInfo activity = app.activityInfo;
+			        	 final ComponentName name = new ComponentName(activity.applicationInfo.packageName, activity.name);
+			        	 shareIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+			        	 shareIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+			        	 shareIntent.setComponent(name);
+			        	 v.getContext().startActivity(shareIntent);
+			        	 break;
+			         }
+			    }
+				
+			}
+		});
+		twitter.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent shareIntent = new Intent(android.content.Intent.ACTION_SEND);
+				shareIntent.setType("text/plain");
+				shareIntent.putExtra(android.content.Intent.EXTRA_TEXT, "I need ya help biatch!");
+				shareIntent.putExtra(Intent.EXTRA_STREAM, storeImage(q.getAdCensored()));
+
+				PackageManager pm = v.getContext().getPackageManager();
+				List<ResolveInfo> activityList = pm.queryIntentActivities(shareIntent, 0);
+			    for (final ResolveInfo app : activityList){
+			         if ((app.activityInfo.name).contains("twitter")){
+			        	 final ActivityInfo activity = app.activityInfo;
+			        	 final ComponentName name = new ComponentName(activity.applicationInfo.packageName, activity.name);
+			        	 shareIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+			        	 shareIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+			        	 shareIntent.setComponent(name);
+			        	 v.getContext().startActivity(shareIntent);
+			        	 break;
+			         }
+			    }
+			}
+		});
+		google.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent shareIntent = new Intent(android.content.Intent.ACTION_SEND);
+				shareIntent.setType("text/plain");
+				shareIntent.putExtra(android.content.Intent.EXTRA_TEXT, "I need ya help biatch!");
+				shareIntent.putExtra(Intent.EXTRA_STREAM, storeImage(q.getAdCensored()));
+
+				PackageManager pm = v.getContext().getPackageManager();
+				List<ResolveInfo> activityList = pm.queryIntentActivities(shareIntent, 0);
+			    for (final ResolveInfo app : activityList){
+			         if ((app.activityInfo.name).contains("twitter")){
+			        	 final ActivityInfo activity = app.activityInfo;
+			        	 final ComponentName name = new ComponentName(activity.applicationInfo.packageName, activity.name);
+			        	 shareIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+			        	 shareIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+			        	 shareIntent.setComponent(name);
+			        	 v.getContext().startActivity(shareIntent);
+			        	 break;
+			         }
+			    }
+			}
+		});
+		
 	} // onCreate
+	
+	private Uri storeImage(Bitmap bm) {
+	    FileOutputStream fileOutputStream = null;
+	    File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+	    File file = new File(path, "test.jpg");
+	    try {
+	        fileOutputStream = new FileOutputStream(file);
+	    } catch (FileNotFoundException e) {
+	        e.printStackTrace();
+	    }
+	    BufferedOutputStream bos = new BufferedOutputStream(fileOutputStream);
+	    bm.compress(CompressFormat.JPEG, 10, bos);
+	    try {
+	        bos.flush();
+	        bos.close();
+	        fileOutputStream.flush();
+	        fileOutputStream.close();
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    }
+	    return Uri.parse("file://" + file.getAbsolutePath());
+	}
+	
+	private void endTask() {
+		questionImageView.setImageBitmap(q.getAdUncensored());
+		LinearLayout page = (LinearLayout) findViewById(R.id.SingleLayout);
+		page.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				finish();
+			}
+		});
+	}
 	
 	// TODO: hier könnte später die jeweilige Frage geladen werden (wenn die nicht direkt übergeben wird)
 	private QuestionSinglePlayer loadQuestion() {
