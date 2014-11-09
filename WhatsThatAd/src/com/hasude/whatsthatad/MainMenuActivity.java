6140d97c26fcf6702e52265a328df4e424221be3
jsuttner <jsuttner@stud.hs-bremen.de>
2014-11-08 16:59:09 -0800

    Rest noch geändert

--- WhatsThatAd/src/com/hasude/whatsthatad/MainMenuActivity.java
+++ WhatsThatAd/src/com/hasude/whatsthatad/MainMenuActivity.java
@@ -3,12 +3,10 @@ package com.hasude.whatsthatad;
 import android.app.Activity;
 import android.os.Bundle;
 
-import com.example.whatsthatad.R;
-
 public class MainMenuActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.single_menu);
+		setContentView(R.layout.activity_single_menu);
 	}
 }
