fcaa6571103361c8d4cf0401f2ac91dae21fc9e9
jsuttner <jsuttner@stud.hs-bremen.de>
2014-11-13 00:05:26 -0800

    SinglePlayerMenu holt die daten von der Datenbank und gibt sie in die Fragments

--- WhatsThatAd/src/com/hasude/whatsthatad/SingleMenuActivity.java
+++ WhatsThatAd/src/com/hasude/whatsthatad/SingleMenuActivity.java
@@ -17,43 +17,25 @@ import android.util.Log;
 
 import com.hasude.whatsthatad.exceptions.CorrectAnswerException;
 import com.hasude.whatsthatad.gameobjects.QuestionSinglePlayer;
-import com.hasude.whatsthatad.sqlite.QuestionDB;
 
 public class SingleMenuActivity extends FragmentActivity implements
 LoaderCallbacks<Cursor>{
 	
 	ViewPager viewPager;
 	SwipeAdapter swipeAdapter;
-	List<QuestionSinglePlayer> labels;
+	List<QuestionSinglePlayer> questionList;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_single_menu);
 		
-		viewPager = (ViewPager) findViewById(R.id.singlePager);
-		swipeAdapter = new SwipeAdapter(getSupportFragmentManager(), 3, viewPager);
-		viewPager.setAdapter(swipeAdapter);
-		
 		// Initialize LoaderManager
 		getLoaderManager().initLoader(0, null, this);
 		
-		// Test Inserts
-		//testInserts();
-	}
-	
-	private void testInserts() {
-		// Creating LocationInsertTask
-		ContentValues contentValues = new ContentValues();
-        contentValues.put(QuestionDB.FIELD_urlCensored, "../test/testCensored.png" );
-        contentValues.put(QuestionDB.FIELD_urlUncensored, "../test/testUncensored.png");
-        contentValues.put(QuestionDB.FIELD_answer, "testanswer");
-        contentValues.put(QuestionDB.FIELD_question, "testquestion?");
-        
-		Log.d("DB", "Inserting test stugg");
-        QuestionInsertTask insertTask = new QuestionInsertTask();
-		insertTask.execute(contentValues);
-		
+		viewPager = (ViewPager) findViewById(R.id.singlePager);
+		swipeAdapter = new SwipeAdapter(getSupportFragmentManager(), 3, questionList);
+		viewPager.setAdapter(swipeAdapter);
 	}
 
 	private class QuestionInsertTask extends AsyncTask<ContentValues, Void, Void>{
@@ -84,13 +66,13 @@ LoaderCallbacks<Cursor>{
 	public void onLoadFinished(Loader<Cursor> arg0, Cursor cursor) {
 		Log.d("DB", "OnLoadFinished aufgerufen");
 
-		labels = new ArrayList<QuestionSinglePlayer>();
+		questionList = new ArrayList<QuestionSinglePlayer>();
 		// looping through all rows and adding to list
 		if (cursor.moveToFirst()) {
 			do {
 				// Try to create new SinglePlayerQuestion and add it to list
 				try {
-					labels.add(new QuestionSinglePlayer(cursor.getInt(0), cursor.getString(1), cursor
+					questionList.add(new QuestionSinglePlayer(cursor.getInt(0), cursor.getString(1), cursor
 							.getString(2), cursor.getString(3), cursor.getString(4)));
 				} catch (CorrectAnswerException e) {
 					e.printStackTrace();
@@ -99,7 +81,7 @@ LoaderCallbacks<Cursor>{
 		}
 		// closing connection
 		
-		for(QuestionSinglePlayer q : labels){
+		for(QuestionSinglePlayer q : questionList){
 			Log.d("DB", "ID: " + q.getAdCensored());
 		}
 		
