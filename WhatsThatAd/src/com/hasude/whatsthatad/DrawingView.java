fa7967bd8f9b8727521172223f4d6757e1c17539
Niklas Hatje <nhatje@gmx.de>
2014-12-02 03:35:18 -0800

    Drawing View

--- WhatsThatAd/src/com/hasude/whatsthatad/DrawingView.java
+++ WhatsThatAd/src/com/hasude/whatsthatad/DrawingView.java
@@ -2,6 +2,7 @@ package com.hasude.whatsthatad;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
 import android.graphics.BitmapFactory;
 import android.graphics.BlurMaskFilter;
 import android.graphics.Canvas;
@@ -11,8 +12,10 @@ import android.graphics.Path;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.LinearLayout.LayoutParams;
@@ -23,6 +26,8 @@ public class DrawingView extends View{
 	
 	//drawing path
 	private Path drawPath;
+	
+	private Path lastPath;
 	//drawing and canvas paint
 	private Paint drawPaint, canvasPaint;
 	//
@@ -33,13 +38,18 @@ public class DrawingView extends View{
 	private Canvas drawCanvas;
 	//canvas bitmap
 	private Bitmap canvasBitmap;
+	// logo bitmap
+	private Bitmap logoBm;
+	private float logoX;
+	private float logoY;
 	
 	private float brushSize, lastBrushSize;
 	
 	private boolean erase=false;
 	
-	public DrawingView(Context context, AttributeSet attrs){
+	public DrawingView(Context context, AttributeSet attrs, Bitmap bm){
 	    super(context, attrs);
+	    canvasBitmap = bm;
 	    this.context = context;
 	    setupDrawing();
 	}
@@ -48,7 +58,7 @@ public class DrawingView extends View{
 		drawPath = new Path();
 		drawPaint = new Paint();
 		
-		drawPaint.setColor(Color.WHITE);
+		drawPaint.setColor(Color.BLACK);
 		drawPaint.setMaskFilter(new BlurMaskFilter(8, BlurMaskFilter.Blur.INNER));
 		
 		drawPaint.setAntiAlias(true);
@@ -63,25 +73,45 @@ public class DrawingView extends View{
 	@Override
 	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
 		super.onSizeChanged(w, h, oldw, oldh);
-		
-		canvasBitmap = BitmapFactory.decodeResource(context.getResources(),
-                R.drawable.adidas_uncensored);
 		canvasBitmap = canvasBitmap.copy(Bitmap.Config.ARGB_8888, true);
 		drawCanvas = new Canvas(canvasBitmap);
 		
 	}
 	
+	 @Override
+     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+         // Compute the height required to render the view
+         // Assume Width will always be MATCH_PARENT.
+         int width = canvasBitmap.getWidth();
+         int height = canvasBitmap.getHeight(); // Since 3000 is bottom of last Rect to be drawn added and 50 for padding.
+         setMeasuredDimension(width, height);
+     }
+	
 	@Override
 	protected void onDraw(Canvas canvas) {
 		canvas.drawBitmap(canvasBitmap, 0, 0, canvasPaint);
+		if (logoBm != null)
+			canvas.drawBitmap(logoBm, logoX, logoY, null);
 		canvas.drawPath(drawPath, drawPaint);
 	}
 	
-	@Override
-	public boolean onTouchEvent(MotionEvent event) {
-		float touchX = event.getX();
-		float touchY = event.getY();
-		
+	public void initLogoBm(Bitmap bm, float left, float top) {
+		if(logoBm == null) {
+			logoBm = bm;
+			logoX = left;
+			logoY = left;
+			invalidate();
+		} else {
+			translateLogoDims(left, top);
+		}
+	}
+	
+	public void updateBm(Bitmap bm) {
+		logoBm = bm;
+	}
+	
+	public boolean touchDraw(float touchX, float touchY, MotionEvent event) {
+
 		switch (event.getAction()) {
 		case MotionEvent.ACTION_DOWN:
 		    drawPath.moveTo(touchX, touchY);
@@ -100,41 +130,11 @@ public class DrawingView extends View{
 		invalidate();
 		return true;
 	}
-	
-	public void setColor(String newColor){
-		//set color   
+
+	public void translateLogoDims(float x, float y) {
+		logoX = x;
+		logoY = y;
 		invalidate();
-		paintColor = Color.parseColor(newColor);
-		drawPaint.setColor(paintColor);
-	}
-	
-	public void setBrushSize(float newSize){
-		//update size
-		float pixelAmount = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 
-			    newSize, getResources().getDisplayMetrics());
-		brushSize=pixelAmount;
-		drawPaint.setStrokeWidth(brushSize);
-	}
-	
-	public void setLastBrushSize(float lastSize){
-	    lastBrushSize=lastSize;
-	}
-	
-	public float getLastBrushSize(){
-	    return lastBrushSize;
-	}
-	
-	public void setErase(boolean isErase){
-		//set erase true or false 
-		erase=isErase;
-		
-		if(erase) drawPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
-		else drawPaint.setXfermode(null);
-	}
-	
-	public void startNew(){
-	    drawCanvas.drawColor(0, PorterDuff.Mode.CLEAR);
-	    invalidate();
 	}
 
 }
